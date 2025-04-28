ef81316bbb1147e224cd69f8d78d52dd83b85e4c
Mina Emad <minaemad6060@gmail.com>
2025-04-28 21:20:36 +0300

    no message

---
+++ Sources/ConvertLocalToLibrary/Resources/Colors+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Colors+Extension.swift
+//  ConvertLocalToLibrary
+//
+//  Created by Mina Emad on 28/04/2025.
+//
+
+import SwiftUI
+
+public extension Color {
+    static let teal100 = Color("teal100", bundle: .module)
+    static let red700 = Color("red700", bundle: .module)
+}
