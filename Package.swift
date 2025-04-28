0b262dd5b0b3be36d6bea287e286b32ced69860d
Mina Emad <minaemad6060@gmail.com>
2025-04-28 20:05:16 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -10,16 +10,18 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ConvertLocalToLibrary",
-            targets: ["ConvertLocalToLibrary"]),
+            targets: ["ConvertLocalToLibrary"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ConvertLocalToLibrary"),
+            name: "ConvertLocalToLibrary",
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .testTarget(
             name: "ConvertLocalToLibraryTests",
             dependencies: ["ConvertLocalToLibrary"]
